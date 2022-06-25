2d4c13ebd08e71329efdaf94bbddd8d665243b15
GreDDySS <misha.sudos@yandex.ru>
2022-06-25 13:43:45 +0800

    🎲 Dice

---
+++ commands/dice.js
@@ -0,0 +1,12 @@
+exports.run = async (client, args, channel, tags, message) => {
+    const dice = Math.floor(Math.random()*6) +1;
+    client.say(channel, `@${user['display-name']}, Результат: ${dice}`);
+}
+
+module.exports.config = {
+    name: "dice",
+    description: "random number from 1 to 6",
+    cooldown: 5000,
+    aliases: ["кубик"],
+    adminOnly: false,
+}
