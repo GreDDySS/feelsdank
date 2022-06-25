5f6d42b23a5e29aa90025b3fbedeb382cf7cd82a
GreDDySS <misha.sudos@yandex.ru>
2022-06-25 13:44:59 +0800

    Template commands

---
+++ commands/template.js
@@ -0,0 +1,11 @@
+exports.run = async (client, args, channel, tags, message) => {
+
+}
+
+module.exports.config = {
+    name: "templ",
+    description: "",
+    cooldown: 5000,
+    aliases: [],
+    adminOnly: false,
+}
