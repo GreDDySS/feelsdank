8ad6185cf269dcbec9fc60ff761763f426872e51
GreDDySS <misha.sudos@yandex.ru>
2022-07-26 20:39:21 +0800

    1

--- commands/restart.js
+++ commands/restart.js
@@ -2,7 +2,6 @@ const {exec} = require("child_process")
 exports.run = async (client, args, channel, tags, message) => {
     await exec("git pull")
     await client.say(channel, '🔁 restart!')
-    await process.exit()
     await exec("npm start")
 }
 
