f769f0bc11ddf279d70f6be02bdf5c406cdde50d
GreDDySS <misha.sudos@yandex.ru>
2022-07-28 17:54:36 +0800

    restart bot

---
+++ commands/restart.js
@@ -0,0 +1,15 @@
+const shell = require("child_process")
+exports.run = async (client, args, channel, tags, message) => {
+    await client.say(channel, `🔁 Restart...`)
+    await shell.exec("git pull")
+    shell.execSync("node bot.js")
+
+}
+
+module.exports.config = {
+    name: "restart",
+    description: "restart bot",
+    cooldown: 5000,
+    aliases: ['rs'],
+    adminOnly: true,
+}
