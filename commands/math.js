89fdcf518a1ff1bc6a4678ec42c05de08cebeb0b
GreDDySS <misha.sudos@yandex.ru>
2022-08-02 01:43:22 +0800

    character limit

--- commands/math.js
+++ commands/math.js
@@ -1,14 +1,21 @@
 const math = require("mathjs")
 exports.run = async (client, args, channel, tags, message) => {
-    const arg = args.join(" ")
-    const result = math.evaluate(arg)
+    const mathem = () => {
+        var result = `${math.evaluate(args.join(' '))}`
+
+        if (result.length > 350) {
+            result = `${result.substring(0, 347)}...`
+        } 
+        return result
+    }
     
+
     if (!args[0]) {
         client.say(channel, "There is no argument.")
         return
     }
 
-    client.say(channel, `@${tags.username}, ${result}`)
+    client.say(channel, `@${tags.username}, ${mathem()}`)
 }
 
 module.exports.config = {
