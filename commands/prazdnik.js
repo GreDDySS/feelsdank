db8befe44485bcc04c467a508382ea0c6f166760
GreDDySS <misha.sudos@yandex.ru>
2022-08-20 21:28:30 +0800

    What a holiday today

--- commands/prazdnik.js
+++ commands/prazdnik.js
@@ -3,20 +3,24 @@ const cheerio = require("cheerio")
 const url = "https://kakoysegodnyaprazdnik.ru"
 exports.run = async (client, args, channel, tags, message) => {
     
-    axios.get("https://kakoysegodnyaprazdnik.ru").then(html => {
+   await axios.get("https://my-calend.ru/holidays").then(html => {
         const $ = cheerio.load(html.data)
         let text = ""
-        $("#main_frame > div > div > div:nth-child(1) > div > a > span").each((i, elem) => {
+        $("body > div.wrapper > main > div.holidays.main > article > section:nth-child(5) > ul > li:nth-child(1) > a").each((i, elem) => {
             text += `${$(elem).text()}`
         })
-        console.log(text)
+        if (text.length > 350) {
+            text = `${result.substring(0, 347)}...`
+        } 
+        client.say(channel, `Сегодняшний праздник: ${text}`)
     })
+
 
 }
 module.exports.config = {
     name: "prazdnik",
-    description: "",
+    description: "What a holiday today",
     cooldown: 5000,
-    aliases: ["test"],
+    aliases: ["праздник"],
     adminOnly: true,
 }
