e0da61b2c03e4c67f7a1b4480d751f91785357ff
GreDDySS <misha.sudos@yandex.ru>
2022-08-21 20:34:27 +0800

    testing commands

---
+++ commands/test.js
@@ -0,0 +1,10 @@
+exports.run = async (client, args, channel, tags, message) => {
+    
+}
+module.exports.config = {
+    name: "test",
+    description: "What a holiday today",
+    cooldown: 10000,
+    aliases: ["test"],
+    adminOnly: true,
+}
