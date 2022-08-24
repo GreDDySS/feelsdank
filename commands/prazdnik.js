29b0a38d59fbff005ca470b038e39d59e65d301f
GreDDySS <misha.sudos@yandex.ru>
2022-08-24 13:04:32 +0800

    upd

--- commands/prazdnik.js
+++ commands/prazdnik.js
@@ -1,6 +1,7 @@
 const rp = require("request-promise")
 const cheerio = require("cheerio")
 const url = "https://kakoyprazdniksegodnya.ru/"
+const user = `Artemu5 BJlaguK_ Bustegg Eggrereal Fedotir GreDDySS GuyRalt Gvardovskiy Gwinsen Iamplugg Iamplugs Matria9 RandomCancer RandomCancer2 StreamElements SunsetColours_ Tuwka_ Xomachel ZULULpa aetenae borobushE crestavlennn eggUrt iLotterytea leonidtelevizor m4x0nn monkeoS oladushek_k rilaveon saopin vexenigmus SunsetColours_ lydeco_ HumanStudi0`
 exports.run = async (client, args, channel, tags, message) => {
     rp(url).then(htm => {
         const $ = cheerio.load(htm)
@@ -8,10 +9,18 @@ exports.run = async (client, args, channel, tags, message) => {
         $("body > div.wrap-page > div.prazdnik-block.prazdnik-block-1 > div > ol > li:nth-child(1)").each((i, elem) => {
             text += `${$(elem).text()}`
         })
+        
         if (text.length > 350) {
-                text = `${result.substring(0, 347)}...`
-            } 
+            text = `${result.substring(0, 347)}...`
+        }
+
+        if(args[0] === "тык") {
+            client.say(channel, `${user} Сегодняшний праздник: ${text}`)
+        } else {
             client.say(channel, `Сегодняшний праздник: ${text}`)
+        }
+        
+            
     })
 
 }
