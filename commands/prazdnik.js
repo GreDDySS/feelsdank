35803519420aa52132f68673fd181a409b98bf77
GreDDySS <misha.sudos@yandex.ru>
2022-08-26 15:50:06 +0800

    kek

--- commands/prazdnik.js
+++ commands/prazdnik.js
@@ -1,12 +1,19 @@
 const rp = require("request-promise")
 const cheerio = require("cheerio")
 const url = "https://kakoyprazdniksegodnya.ru/"
-const user = `Artemu5 BJlaguK_ Bustegg Eggrereal Fedotir GreDDySS GuyRalt Gvardovskiy Gwinsen Iamplugg Iamplugs Matria9 RandomCancer RandomCancer2 StreamElements SunsetColours_ Tuwka_ Xomachel ZULULpa aetenae borobushE crestavlennn eggUrt iLotterytea leonidtelevizor m4x0nn monkeoS oladushek_k rilaveon saopin vexenigmus SunsetColours_ lydeco_ HumanStudi0`
+const user = `Artemu5 BJlaguK_ Bustegg Eggrereal Fedotir GreDDySS GuyRalt Gvardovskiy Gwinsen Iamplugg Iamplugs Matria9 RandomCancer RandomCancer2 StreamElements SunsetColours_ Tuwka_ Xomachel ZULULpa aetenae borobushE crestavlennn eggUrt iLotterytea leonidtelevizor m4x0nn monkeoS oladushek_k rilaveon saopin vexenigmus SunsetColours_ lydeco_ HumanStudi0 AlexanderLer Nipropieren`
+const selector = 
+["body > div.wrap-page > div.prazdnik-block.prazdnik-block-1 > div > ol > li:nth-child(1)",
+"body > div.wrap-page > div.prazdnik-block.prazdnik-block-1 > div > ol > li:nth-child(2)",
+"body > div.wrap-page > div.prazdnik-block.prazdnik-block-1 > div > ol > li:nth-child(3)",
+"body > div.wrap-page > div.prazdnik-block.prazdnik-block-1 > div > ol > li:nth-child(4)",
+"body > div.wrap-page > div.prazdnik-block.prazdnik-block-1 > div > ol > li:nth-child(5)"
+]
 exports.run = async (client, args, channel, tags, message) => {
     rp(url).then(htm => {
         const $ = cheerio.load(htm)
         let text = ""
-        $("body > div.wrap-page > div.prazdnik-block.prazdnik-block-1 > div > ol > li:nth-child(1)").each((i, elem) => {
+        $(feelsdank.Misc.randomArg(selector)).each((i, elem) => {
             text += `${$(elem).text()}`
         })
         
@@ -28,6 +35,6 @@ module.exports.config = {
     name: "prazdnik",
     description: "What a holiday today",
     cooldown: 10000,
-    aliases: ["праздник"],
+    aliases: ["празник"],
     adminOnly: false,
 }
