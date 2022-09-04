751ff1646eb2254e0517c4f0958405d5ff1abe1d
GreDDySS <misha.sudos@yandex.ru>
2022-09-05 01:13:39 +0800

    upd

--- commands/sim.js
+++ commands/sim.js
@@ -1,6 +1,7 @@
 const rp = require("request-promise")
 const cheerio = require("cheerio")
 const url = "https://ilotterytea.itch.io/maxon"
+const ur = "ilotterytea.itch.io/maxon"
 exports.run = async (client, args, channel, tags, message) => {
     rp(url).then(htm => {
         const $ = cheerio.load(htm)
@@ -10,16 +11,19 @@ exports.run = async (client, args, channel, tags, message) => {
           text += `${$(elem).text()}`
         })
         
+        if(args[0] === "link") {
+            client.say(channel, `${tags["display-name"]}, Ссылка на симулятор: ${ur}`)
+        } else {
          client.say(channel, `Версия Симулятора: ${text}`)
-        
+        }
             
     })
 // #view_game_page_86405 > div.columns > div.left_col.column > div.uploads > div > div > div > div > strong
 }
 module.exports.config = {
-    name: "sim",
-    description: "kek",
+    name: "simulator",
+    description: "check version maxon petting simulator",
     cooldown: 10000,
-    aliases: ["simulator", "maxon"],
+    aliases: ["sim", "maxon"],
     adminOnly: false,
 }
