55e62b569697f0f46df11e75c652d456619caf33
GreDDySS <misha.sudos@yandex.ru>
2022-09-12 18:44:51 +0800

    fix selectors

--- commands/prazdnik.js
+++ commands/prazdnik.js
@@ -3,12 +3,12 @@ const cheerio = require("cheerio")
 const url = "https://my-calend.ru/holidays"
 const user = `Artemu5 BJlaguK_ Bustegg Eggrereal Fedotir GreDDySS GuyRalt Gvardovskiy Gwinsen Iamplugg Iamplugs Matria9 RandomCancer RandomCancer2 StreamElements SunsetColours_ Tuwka_ Xomachel ZULULpa aetenae borobushE crestavlennn eggUrt iLotterytea leonidtelevizor m4x0nn monkeoS oladushegg_ rilaveon saopin vexenigmus SunsetColours_ lydeco_ HumanStudi0 AlexanderLer Nipropieren`
 const selector = 
-["body > div.wrapper > main > div.holidays.main > article > section:nth-child(5) > ul > li:nth-child(1) > a",
-"body > div.wrapper > main > div.holidays.main > article > section:nth-child(5) > ul > li:nth-child(2) > a",
-"body > div.wrapper > main > div.holidays.main > article > section:nth-child(5) > ul > li:nth-child(3) > a",
-"body > div.wrapper > main > div.holidays.main > article > section:nth-child(5) > ul > li:nth-child(4) > a",
-"body > div.wrapper > main > div.holidays.main > article > section:nth-child(5) > ul > li:nth-child(5) > a",
-"body > div.wrapper > main > div.holidays.main > article > section:nth-child(5) > ul > li:nth-child(6) > a"
+["body > div.wrapper > main > div.holidays.main > article > section:nth-child(5) > ul > li:nth-child(1) ",
+"body > div.wrapper > main > div.holidays.main > article > section:nth-child(5) > ul > li:nth-child(2) ",
+"body > div.wrapper > main > div.holidays.main > article > section:nth-child(5) > ul > li:nth-child(3) ",
+"body > div.wrapper > main > div.holidays.main > article > section:nth-child(5) > ul > li:nth-child(4)",
+"body > div.wrapper > main > div.holidays.main > article > section:nth-child(5) > ul > li:nth-child(5)",
+"body > div.wrapper > main > div.holidays.main > article > section:nth-child(5) > ul > li:nth-child(6)"
 ]
 exports.run = async (client, args, channel, tags, message) => {
     rp(url).then(htm => {
