0f25862124dd162e99247904c72e01dec25c1762
GreDDySS <misha.sudos@yandex.ru>
2023-01-27 17:44:40 +0800

    rename and add a static parsing file

---
+++ commands/celebration.js
@@ -0,0 +1,20 @@
+const user = `Artemu5 BJlaguK_ Bustegg Eggrereal Fedotir GreDDySS GuyRalt Gvardovskiy Gwinsen Iamplugg Iamplugs Matria9 RandomCancer RandomCancer2 StreamElements SunsetColours_ Tuwka_ Xomachel ZULULpa aetenae borobushE crestavlennn eggUrt iLotterytea BetterCallTelevizor m4x0nn monkeoS oladushegg_ rilaveon saopin vexenigmus SunsetColours_ lydeco_ HumanStudi0 AlexanderLer Nipropieren`
+var data = require("../other/celebration.json")
+exports.run = async (client, args, channel, tags, message) => {
+    const num = feelsdank.Misc.random(data.length)
+    if(args[0] === "тык" && channel === "iamplugg") {
+        return client.say(channel, `${user} Сегодняшний праздник: ${data[num - 1]}`)
+    }
+    if(args[0] === "info") {
+        return client.say(channel, `Сегодняшний праздник: ${data[num - 1]} — ${num} из ${data.length}`)
+    } else {
+        return client.say(channel, `Сегодняшний праздник: ${data[num - 1]}`)
+    }
+}
+module.exports.config = {
+    name: "prazdnik",
+    description: "What a holiday today",
+    cooldown: 5000,
+    aliases: ["праздник"],
+    adminOnly: false,
+}
