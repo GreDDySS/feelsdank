b37a058b83a3e6445dd985d6217a5ed1815b7bca
GreDDySS <misha.sudos@yandex.ru>
2023-04-03 18:15:58 +0800

    ...

--- commands/celebration.js
+++ commands/celebration.js
@@ -1,7 +1,8 @@
+const fs = require("fs")
 const user = `Artemu5 BJlaguK_ Bustegg Eggrereal Fedotir GreDDySS GuyRalt Gvardovskiy Gwinsen Iamplugg Iamplugs Matria9 RandomCancer RandomCancer2 StreamElements SunsetColours_ Tuwka_ Xomachel ZULULpa aetenae borobushE crestavlennn eggUrt iLotterytea BetterCallTelevizor m4x0nn monkeoS oladushegg_ rilaveon saopin vexenigmus SunsetColours_ lydeco_ HumanStudi0 AlexanderLer Nipropieren`
 exports.run = async (client, args, channel, tags, message) => {
-    const num = feelsdank.Misc.random(data.length)
     const data = JSON.parse(fs.readFileSync("other/celebration.json", {encoding: "utf-8"}))
+    const num = feelsdank.Misc.random(data.length)
     if(args[0] === "тык" && channel === "iamplugg") {
         return client.say(channel, `${user} Сегодняшний праздник: ${data[num - 1]}`)
     }
